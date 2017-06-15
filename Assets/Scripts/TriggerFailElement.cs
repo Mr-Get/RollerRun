46825c4a4fe7d0434e127c885619c1cdbd414205
Sebastian Wanke <sebastian.wanke@gmx.at>
2017-06-15 14:36:29 +0200

    Duplicator added

--- Assets/Scripts/TriggerFailElement.cs
+++ Assets/Scripts/TriggerFailElement.cs
@@ -2,22 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerFailElement : MonoBehaviour {
+public class TriggerFailElement : MonoBehaviour
+{
 
     GameObject gameBall;
 
     // Use this for initialization
-    void Start () {
-       gameBall = GameObject.Find("GameBall");
+    void Start()
+    {
+        gameBall = GameObject.Find("GameBall");
 
     }
 
     // Update is called once per frame
-    void Update () {
-		
-	}
-    void OnTriggerExit(Collider col) { 
-        if(col.gameObject.name.IndexOf("Ball")> -1 && gameBall.GetComponent<CollectPoints>().effect != "shieldActivate")
+    void Update()
+    {
+
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.name.IndexOf("Ball") > -1 && gameBall.GetComponent<CollectPoints>().effect != "shieldActivate")
         {
             Destroy(col.gameObject);
         }
