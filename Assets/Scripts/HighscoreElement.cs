26f5d489b01163617939fe52a34ef411f888e6b9
Sebastian Wanke <sebastian.wanke@gmx.at>
2017-06-18 23:01:44 +0200

    Highscore nearly done

---
+++ Assets/Scripts/HighscoreElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighscoreElement : MonoBehaviour {
+
+    public int rowCount;
+    public GameObject[] children;
+
+	// Use this for initialization
+	void Start () {
+        children = this.gameObject.GetComponentsInChildren<GameObject>();
+        int i = 0;
+        Debug.Log(children.Length);
+        foreach(GameObject g in children)
+        {
+            Debug.Log(i + " " + g.name);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
